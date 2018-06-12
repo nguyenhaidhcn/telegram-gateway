469f222c5432d8412d499e75736602eb7bb01242
haint <nguyenhaidhcn@gmail.com>
2018-06-12 16:08:16 +0700

    binance

--- src/main/java/com/snap/gateway/handler/binance/BinanceHandlerCondition.java
+++ src/main/java/com/snap/gateway/handler/binance/BinanceHandlerCondition.java
@@ -11,7 +11,7 @@ public class BinanceHandlerCondition implements Condition {
 	@Override
 	public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
 		String gatewayName = context.getEnvironment().getProperty("gateway.name");
-		return Gateways.kucoin.value().equalsIgnoreCase(gatewayName);
+		return Gateways.binance.value().equalsIgnoreCase(gatewayName);
 	}
 
 }
