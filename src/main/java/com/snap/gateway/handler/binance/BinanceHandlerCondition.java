470b23862ed593ce104fdf8f54067d4a635cb776
haint <nguyenhaidhcn@gmail.com>
2018-06-12 15:51:11 +0700

    binance

---
+++ src/main/java/com/snap/gateway/handler/binance/BinanceHandlerCondition.java
@@ -0,0 +1,17 @@
+package com.snap.gateway.handler.binance;
+
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+import com.snap.gateway.common.Gateways;
+
+public class BinanceHandlerCondition implements Condition {
+
+	@Override
+	public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+		String gatewayName = context.getEnvironment().getProperty("gateway.name");
+		return Gateways.kucoin.value().equalsIgnoreCase(gatewayName);
+	}
+
+}
